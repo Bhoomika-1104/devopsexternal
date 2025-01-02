d150bb3d86025e3d6b47a39981c0b51e2a4a045c
Bhoomika-1104 <devipriyabhoomikacharugundla@gmail.com>
2025-01-02 11:58:25 +0530

    external1

---
+++ add1.java
@@ -0,0 +1,21 @@
+
+class AddTwoNumbers {
+    public static void main(String[] args) {
+       
+        System.out.print("Enter first number: ");
+        int num1 = 10;
+
+        // Input second number
+        System.out.print("Enter second number: ");
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner
+        //scanner.close();
+    }
+}
